8e25339fd36fb3fa9a913915ea76781cfaa0c72a
Komar Jim <a1500603@myy.haaga-helia.fi>
2017-10-06 15:15:50 +0300

    security 2

--- src/main/java/fi/haagahelia/course/Bookstore/BookstoreApplication.java
+++ src/main/java/fi/haagahelia/course/Bookstore/BookstoreApplication.java
@@ -11,6 +11,8 @@ import fi.haagahelia.course.Bookstore.domain.Book;
 import fi.haagahelia.course.Bookstore.domain.BookRepository;
 import fi.haagahelia.course.Bookstore.domain.Category;
 import fi.haagahelia.course.Bookstore.domain.CategoryRepository;
+import fi.haagahelia.course.Bookstore.domain.User;
+import fi.haagahelia.course.Bookstore.domain.UserRepository;
 
 @SpringBootApplication
 public class BookstoreApplication {
@@ -21,7 +23,7 @@ public class BookstoreApplication {
 	}
 
 	@Bean
-	public CommandLineRunner studentDemo(BookRepository repository, CategoryRepository crepository) {
+	public CommandLineRunner studentDemo(BookRepository repository, CategoryRepository crepository, UserRepository urepository) {
 		return (args) -> {
 			log.info("save a couple of books");
 			crepository.save(new Category("Fantasy"));
@@ -33,6 +35,12 @@ public class BookstoreApplication {
 					crepository.findByName("History").get(0)));
 			repository.save(new Book("War and Peace", "Leo Tolstoy", 1867, "6546", 20,
 					crepository.findByName("History").get(0)));
+
+			// Create users: admin/admin user/user
+			User user1 = new User("user", "$2a$06$OZVm2CzF.vx1bb2XZcvSReBIUJOw77UIpEY8GFx9.HGL3gyhgcaSa", "user@mail.com", "USER");
+			User user2 = new User("admin", "$2a$06$fClI3fmnqZPuWcB7Ac40bOwku33v124kdtl2wSwcUiguX.STIYpbG", "admin@mail.com", "ADMIN");
+			urepository.save(user1);
+			urepository.save(user2);
 
 			log.info("fetch all books");
 			for (Book book : repository.findAll()) {
